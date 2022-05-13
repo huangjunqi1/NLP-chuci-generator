fe4b629165c7b33f536d9bbffd2865d12e3ff739
huangjunqi1 <1017728803@qq.com>
2022-05-14 00:20:04 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from curses import termattrs
+from tkinter.tix import Tree
 import torch
 import torch.nn as nn
 import random
@@ -30,8 +32,9 @@ class S2SModel(nn.Module):
         self.voc_size = voc_size
         self.max_len = max_length
         self.encoder = Encoder(voc_size,input_size,hidden_size,n_layers)
+        self.decoder = AttentionDecoder(voc_size,input_size,hidden_size,hidden_size,n_layers)
         self.sep_id = sep_id
-    def forward(self,inputs,hidden=None,tagets=None,teacher_force_ratio=0.5):
+    def forward(self,inputs,hidden=None,targets=None,teacher_force_ratio=0.5):
         num_sents = inputs.size(1)
         outputs = torch.zeros(inputs.size(0),inputs.size(1),self.max_len,self.voc_size,device=inputs.device)
         enc_hidden = None
@@ -40,3 +43,25 @@ class S2SModel(nn.Module):
         for sent_id in range(num_sents):
             if sent_id > 0
                 enc_outpus,enc_hidden = self.encoder(enc_inputs,enc_hidden)
+            input = inputs[:,sent_id,0]
+            enc_inputs = torch.zeros(inputs.size(0),self.max_len + 1, dtype = torch.long, device = inputs.device)
+            enc_inputs[:,0] = input
+            i = 0 
+            while (True):
+                output,hidden = self.encoder(input.unsqueeze(1),hidden,enc_outputs)
+                outputs[:,sent_id,i,:] = output(:,0,:)
+                input = (targets[:,sent_id,i] if targets is not None and random.random() < teacher_force_ratio else output.argmax(2))
+                enc_inputs[:,i+1] = input
+                if (input == end_id) : break
+            sep_id = self.sep_id[sent_id % 2]
+            input = torch.tensor(sep_id , dtype = torch.long, device = inputs.device).expand_as(input)
+            enc_inputs[:,self.max_len] = input
+            output,hidden = self.decoder(input,unsqueeeze(1),hidden,enc_outputs) 
+
+
+
+class AttentionDecoder(nn.Module):
+    def __init__(self,voc_size,input_size,encoutput_size,hidden_size,n_layers):
+        super.__init__()
+    def forward(self,input,dec_hidden=None,enc_output=None)
+        if (len(input.size())==1) input.unsqueeze_(1)
