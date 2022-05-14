f72dc9728d89787517ff6abdc4f3826b53b03ad1
huangjunqi1 <1017728803@qq.com>
2022-05-14 09:43:48 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+# set maxlen of a sequence,batch_size,teacher_for_ratio
+max_len = 8 #set maxlen of sequences
+batch_size = 10 #set batch size
+teacher_for_ratio = 0.5 #set teacher_for_ratio
+drop = nn.Dropout(0.5)
+#config = Config(s_maxlen,s_batch_size,s_teacher_for_ratio)
+
