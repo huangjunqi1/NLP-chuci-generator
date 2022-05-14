22c227619b7cb67569533ed127ee6d638d0333df
huangjunqi1 <1017728803@qq.com>
2022-05-14 09:57:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,7 @@ max_len = 8 #set maxlen of sequences
 batch_size = 10 #set batch size
 teacher_for_ratio = 0.5 #set teacher_for_ratio
 drop = nn.Dropout(0.5)
+Eos = 0 # set EOS id
+Pad = 1 # set padding id
 #config = Config(s_maxlen,s_batch_size,s_teacher_for_ratio)
 
