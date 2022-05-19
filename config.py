041a51c7257fcec3476b358fa1e57fdabb0fe026
huangjunqi1 <1017728803@qq.com>
2022-05-19 15:40:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ batch_size = 10 #set batch size
 teacher_for_ratio = 0.5 #set teacher_for_ratio
 drop = nn.Dropout(0.5)
 comma = dataloader.comma # set id of ,
-dot = dataloader.dot
-Pad = 1 # set padding id
+dot = dataloader.dot # set id of 。
+Pad = dataloader.pad # set padding id
+Sos = dataloader.sos # set start of a sentence
 #config = Config(s_maxlen,s_batch_size,s_teacher_for_ratio)
 
