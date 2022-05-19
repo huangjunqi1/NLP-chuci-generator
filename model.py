5077b1fd549fb67144ca3ef0d99cf8f6d59f76bf
huangjunqi1 <1017728803@qq.com>
2022-05-19 17:53:02 +0800

    Update model.py

--- model.py
+++ model.py
@@ -52,13 +52,13 @@ class S2SModel(nn.Module):
         outputs = torch.zeros(inputs.size(0),inputs.size(1),self.max_len,self.voc_size,device=inputs.device)
         enc_hidden = None
         enc_outputs = None
-        key_padding_mask = torch.zeors(batch_size,self.max_len,dtype=torch.bool)
+        key_padding_mask = torch.zeros(batch_size,self.max_len,dtype=torch.bool)
         for sent_id in range(num_sents):
             if (sent_id == 0):
                 enc_inputs = inputs[:,0,:]
                 for i in range(batch_size):
                     for j in range(self.max_len):
-                        if (inputs[i][0][j]==Pad): key_padding_mask[i][j] = True
+                        if (inputs[i][0][j].item()==Pad): key_padding_mask[i][j] = True
                 continue
             enc_outputs,enc_hidden = self.encoder(enc_inputs,enc_hidden)
             input = torch.LongTensor([[Sos]]*batch_size)
