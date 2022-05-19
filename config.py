a049fcf22e37b36d8566d8e29274f41e2cd0e80f
huangjunqi1 <1017728803@qq.com>
2022-05-19 16:25:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import dataloader
 
 # set maxlen of a sequence,batch_size,teacher_for_ratio
-max_len = 8 #set maxlen of sequences
+max_len = dataloader.maxlen #set maxlen of sequences
 batch_size = 10 #set batch size
 teacher_for_ratio = 0.5 #set teacher_for_ratio
 drop = nn.Dropout(0.5)
