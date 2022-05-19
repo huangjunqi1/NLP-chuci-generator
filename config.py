d588ae38213cabb9904827495e2b235a9328d70b
huangjunqi1 <1017728803@qq.com>
2022-05-19 15:13:13 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
-
+import dataloader
 
 # set maxlen of a sequence,batch_size,teacher_for_ratio
 max_len = 8 #set maxlen of sequences
 batch_size = 10 #set batch size
 teacher_for_ratio = 0.5 #set teacher_for_ratio
 drop = nn.Dropout(0.5)
-Eos = 0 # set EOS id
+comma = dataloader.comma # set id of ,
+dot = dataloader.dot
 Pad = 1 # set padding id
 #config = Config(s_maxlen,s_batch_size,s_teacher_for_ratio)
 
